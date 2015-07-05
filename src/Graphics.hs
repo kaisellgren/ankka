028de05f7a3691c0a0146c7b60c435dab10137f1
Kai Sellgren <kaisellgren@gmail.com>
2015-07-05 18:13:39 +0300

    Move vertex2 and vector3 functions to Graphics

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -6,3 +6,9 @@ import qualified Graphics.Rendering.OpenGL as GL
 renderTriangle :: [Vector2] -> IO ()
 renderTriangle vertices =
     GL.renderPrimitive GL.Triangles $ mapM_ (GL.vertex . vertex2) vertices
+
+vector3 :: Vector2 -> GL.Vector3 GL.GLfloat
+vector3 (x, y) = GL.Vector3 (realToFrac x) (realToFrac y) 0
+
+vertex2 :: Vector2 -> GL.Vertex2 GL.GLfloat
+vertex2 (x, y) = GL.Vertex2 (realToFrac x) (realToFrac y)
--- src/Math.hs
+++ src/Math.hs
@@ -1,7 +1,5 @@
 module Math where
 
-import qualified Graphics.Rendering.OpenGL as GL
-
 type Vector2 = (Float, Float)
 
 fromAngle :: Float -> Vector2
@@ -30,9 +28,3 @@ vdot (x1, y1) (x2, y2) = x1 * x2 + y1 * y2
 
 vscalar :: Vector2 -> Vector2 -> Float
 vscalar (x1, y1) (x2, y2) = (x1 * y2) - (x2 * y1)
-
-vector3 :: Vector2 -> GL.Vector3 GL.GLfloat
-vector3 (x, y) = GL.Vector3 (realToFrac x) (realToFrac y) 0
-
-vertex2 :: Vector2 -> GL.Vertex2 GL.GLfloat
-vertex2 (x, y) = GL.Vertex2 (realToFrac x) (realToFrac y)
