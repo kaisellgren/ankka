17e939d5ad5a7fc13fae2c1f97d7caf0efbcce4a
Kai Sellgren <kaisellgren@gmail.com>
2015-07-06 12:55:51 +0300

    Remove redundant parens

--- src/Math.hs
+++ src/Math.hs
@@ -21,7 +21,7 @@ vscale :: Vector2 -> Float -> Vector2
 vscale (x, y) s = (x * s, y * s)
 
 vunit :: Vector2 -> Vector2
-vunit v = vscale v $ (1 / vlength v)
+vunit v = vscale v $ 1 / vlength v
 
 vdot :: Vector2 -> Vector2 -> Float
 vdot (x1, y1) (x2, y2) = x1 * x2 + y1 * y2
