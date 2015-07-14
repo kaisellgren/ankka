1473d5b6254c6ca7dbd2d2ff37efeef8b13e1550
Kai Sellgren <kaisellgren@gmail.com>
2015-07-14 17:05:56 +0300

    Clean up the mess I made

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -17,19 +17,17 @@ renderEntity :: Entity -> IO ()
 renderEntity e = do
     GL.activeTexture GL.$= GL.TextureUnit 0
     GL.textureBinding GL.Texture2D GL.$= Just (texture $ model e)
-    let
-        glSize = GL.TextureSize2D (gsizei $ fst size) (gsizei $ snd size)
-    GL.texImage2D Nothing GL.NoProxy 0 GL.RGB8 glSize 0 (GL.PixelData GL.BGR GL.UnsignedByte bPtr)
-    renderTriangle $ fmap (vadd $ position e) (points $ model e :: [Vector2])
     GL.renderPrimitive GL.Quads $ do
-        n 0 1 0
-        t 0 1 >> v 10 (-10) 10
-        t 1 1 >> v 10 (-10) (-10)
-        t 1 0 >> v (-10) (-10) (-10)
-        t 0 0 >> v (-10) (-10) 10
-      where v x y z = GL.vertex (GL.Vertex3 x y z :: GL.Vertex3 GL.GLfloat)
-            n x y z = GL.normal (GL.Normal3 x y z :: GL.Normal3 GL.GLfloat)
-            t u v = GL.texCoord (GL.TexCoord2 u v :: GL.TexCoord2 GL.GLfloat)
+       v 100 100
+       t 0 1
+       v 100 (-100)
+       t 1 1
+       v (-100) (-100)
+       t 1 0
+       v (-100) 100
+       t 0 0
+     where v x y = GL.vertex (GL.Vertex2 x y :: GL.Vertex2 GL.GLfloat)
+           t u v = GL.texCoord (GL.TexCoord2 u v :: GL.TexCoord2 GL.GLfloat)
 
 vector3 :: Vector2 -> GL.Vector3 GL.GLfloat
 vector3 (x, y) = GL.Vector3 (realToFrac x) (realToFrac y) 0
